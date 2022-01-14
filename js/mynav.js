cf6b31c2492025d179f786949ad0885b08e57989
Prasanna <prasannaps2610@gmail.com>
2022-01-14 16:52:34 +0530

    Navbar clicks handling

---
+++ js/mynav.js
@@ -0,0 +1,32 @@
+function fixResizeAnimations() {
+  let resizeTimer;
+  window.addEventListener('resize', () => {
+    document.body.classList.add('resize-animation-stopper');
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(() => {
+      document.body.classList.remove('resize-animation-stopper');
+    }, 400);
+  });
+}
+
+const NavigationBar = (function () {
+  const slider = document.getElementById('nav-items-holder'),
+    hamburgerButton = document.getElementById('ham-burger-button'),
+    secondContainer = document.getElementById('second-container');
+
+  function toggleSideBar() {
+    slider.classList.toggle('active');
+    hamburgerButton.classList.toggle('active');
+  }
+
+  slider.addEventListener('click', e => {
+    e.stopPropagation();
+  });
+
+  hamburgerButton.addEventListener('click', toggleSideBar);
+  secondContainer.addEventListener('click', toggleSideBar);
+
+  return { toggleSideBar };
+})();
+
+fixResizeAnimations();
