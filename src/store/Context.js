868f32d406c2b7d6a4f45ce8407f03328dc85a1a
Prasanna Ps <prasannaps2610@gmail.com>
2024-01-29 22:13:59 +0530

    Formatting and cleanup

--- src/store/Context.js
+++ src/store/Context.js
@@ -1,5 +1,6 @@
-import { atom } from "nanostores";
+import { atom } from 'nanostores';
 
 export const isHeroAnimPending = atom(true);
 export const currentPageIndex = atom(0);
 export const isInContactPage = atom(false);
+export const isHamburgerOn = atom(false);
--- src/store/mailIdCopied.js
+++
@@ -1,3 +0,0 @@
-import { atom } from "nanostores";
-
-export default atom(false);
