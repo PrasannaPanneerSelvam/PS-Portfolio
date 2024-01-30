f5a1bb125528e980e3c947dc6d2befaf7ac692d0
Prasanna Ps <prasannaps2610@gmail.com>
2024-01-30 09:22:53 +0530

    Removed nano store & added local implementation

--- src/store/Context.js
+++ src/store/Context.js
@@ -1,6 +1,34 @@
-import { atom } from 'nanostores';
+// import { atom } from "nanostores";
 
-export const isHeroAnimPending = atom(true);
-export const currentPageIndex = atom(0);
-export const isInContactPage = atom(false);
-export const isHamburgerOn = atom(false);
+const atom = function (initialValue) {
+  let store = initialValue;
+  const cbArr = [];
+
+  return {
+    get: function () {
+      return store;
+    },
+    set: function (val) {
+      if (store === val) return;
+      store = val;
+      cbArr.forEach((i) => i(store));
+    },
+    subscribe: function (cb) {
+      cbArr.push(cb);
+    },
+  };
+};
+
+const isHeroAnimPending = atom(true),
+  currentPageIndex = atom(0);
+
+const isInContactPage = atom(false);
+
+const isInContactPageSetter = isInContactPage.set;
+currentPageIndex.subscribe(function (idx) {
+  isInContactPageSetter(idx === 2);
+});
+
+delete isInContactPage["set"];
+
+export { currentPageIndex, isHeroAnimPending, isInContactPage };
