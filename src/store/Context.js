fe24f2aa42c40804be0ed4139fac97098b729f5c
Prasanna Ps <prasannaps2610@gmail.com>
2024-01-31 19:10:31 +0530

    Pub sub model cleanup

--- src/store/Context.js
+++ src/store/Context.js
@@ -1,4 +1,5 @@
 // import { atom } from "nanostores";
+import sectionNames from "../sectionNames";
 
 const atom = function (initialValue) {
   let store = initialValue;
@@ -13,24 +14,27 @@ const atom = function (initialValue) {
       store = val;
       cbArr.forEach((i) => i(store));
     },
-    subscribe: function (cb) {
+    subscribe: function (cb, callCbImmediatelyWithCurrentValue = true) {
       cbArr.push(cb);
+      callCbImmediatelyWithCurrentValue && cb(store);
     },
   };
 };
 
-const isHeroAnimPending = atom(true),
-  currentPageIndex = atom(0);
+const currentHash = window.location.hash;
 
-const isInContactPage = atom(false);
+let pageIndex = 0;
+if (currentHash !== "") {
+  const sectionsIds = sectionNames.map((i) => i.toLowerCase());
+  pageIndex = sectionsIds.indexOf(currentHash.slice(1));
+  if (~pageIndex) {
+    document.getElementById(sectionsIds[pageIndex]).scrollIntoView();
+  } else {
+    pageIndex = 0;
+  }
+}
 
-const isInContactPageSetter = isInContactPage.set;
-currentPageIndex.subscribe(function (idx) {
-  isInContactPageSetter(idx === 2);
-});
+const isHeroAnimPending = atom(pageIndex === 0),
+  currentPageIndex = atom(pageIndex);
 
-delete isInContactPage["set"];
-
-export { currentPageIndex, isHeroAnimPending, isInContactPage };
-
-export const currentHash = window.location.hash;
+export { currentPageIndex, isHeroAnimPending };
